39fb631689abfefd2001af84bd0c383fac755382
JackLiang1 <ideabreeze@gmail.com>
2016-02-04 01:42:25 -0600

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -5,6 +5,14 @@ import pandas as pd
 today=date.today()
 start=(today.year-1,today.month,today.day)
 quotes=quotes_historical_yahoo('AXP',start,today)
-df=pd.DataFrame(quotes)
+fields=['date','open','close','high','low','volume']
+list1=[]
+for i in range(0,len(quotes)):
+  x=date.fromordinal(int(quote[i][0]))
+  y=datetime.strftime(x'%Y-%m-%d')
+  list1.append(y)
+quotesdf=pd.DataFrame(quotes,index=list1,columns=fields)
+quotesdf=quotesdf.drop(['date'],axis=1)
 
-print df
+
+print quotesdf
